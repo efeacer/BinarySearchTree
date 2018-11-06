0cd4bb01a6b0258e99dfe36e3fa52125d54992fe
Efe Acer <efeacer@gmail.com>
2018-11-06 01:44:40 +0100

    Define BinarySearchTreeInterface

---
+++ src/BinarySearchTreeInterface.java
@@ -0,0 +1,21 @@
+public interface BinarySearchTreeInterface<T> {
+
+    public boolean isEmpty();
+
+    public int size();
+
+    public boolean contains(T item);
+
+    public T retrieve(T item);
+
+    public T delete(T item);
+
+    public void insert(T item);
+
+    public void inorderTraverse();
+
+    public void preorderTraverse();
+
+    public void postorderTraverse();
+
+}
